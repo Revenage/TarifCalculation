73fc06a177a39e479b9190ba04b3ad90fa314bed
Revenage <romanvolobuev93@gmail.com>
2015-07-16 14:25:21 +0300

    fix table

--- js/Services/userData.js
+++ js/Services/userData.js
@@ -9,7 +9,7 @@ app.service('userData',function($rootScope){
     items:[
               {name: 'Телевизор',         color: "#DD0000", col: 2, time: 240,  power: 100 },
               {name: 'Стиральная машина', color: "#FF4500", col: 1, time: 10,   power: 1500},
-              {name: 'Холодильник',       color: "#11325B", col: 1, time: 950, power: 700 },
+              {name: 'Холодильник',       color: "#33546D", col: 1, time: 950, power: 700 },
               {name: 'Утюг',              color: "#006400", col: 1, time: 20,   power: 2000},
               {name: 'Электрочайник',     color: "#2a7aff", col: 1, time: 40,   power: 1000}
           ],
